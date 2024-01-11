cdfe6b957ffddaa6ecb2907106c71b6856b6a846
mdkh9980 <mdkh.9980@gmail.com>
2024-01-11 22:07:27 +0530

    gsap commit

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
+import gsap from "gsap";
+
 document.addEventListener("DOMContentLoaded", function(){
     const button = document.querySelector("#navBtn");
-    // if(!button) return;
+    if(!button) return;
     button.addEventListener("click", function(){
     const heading = document.querySelector("#heading");
     if(!heading) return;
     heading.textContent = "Built by file";
+    gsap.to("#heading", {
+        x:300,
+        duration:3,
+        delay:1
+    })
     });
 })
